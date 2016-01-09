330973d20450c64cd475ec1b40aa62fbd37768e3
Jonathan Sampson <jjdsampson@gmail.com>
2016-01-08 17:12:03 -0800

    Demo showing multiple sourcemap comments
    Edge and Internet Explorer will not download the sourcemap file when
    multiple comments are encountered.

---
+++ nsourcemaps/index.js
@@ -0,0 +1,4 @@
+var say = function (m) { return document.body.textContent = m; };
+say("Hello, World.");
+//# sourceMappingURL=index.js.map
+//# sourceMappingURL=index.js.map
---
+++ nsourcemaps/index.ts
@@ -0,0 +1,2 @@
+let say = m => document.body.textContent = m;
+say( "Hello, World." );
